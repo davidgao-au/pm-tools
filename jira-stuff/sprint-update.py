ce5213b9b9973526e7bbb503c501279e7fccad52
David Gao <david.gao@autonomic.ai>
2022-12-06 08:25:59 +0800

    Put almost everything in a class

--- jira-stuff/sprint-update.py
+++ jira-stuff/sprint-update.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import time
 import json
@@ -16,33 +17,36 @@ from jira.client import ResultList
 from jira.resources import Issue
 
 
+def create_session():
+    email = os.environ.get('JIRA_USER_NAME')
+    api_token = os.environ.get('JIRA_API_TOKEN')
+
+    if not email:
+        print("Missing Jira user name!")
+        exit(1)
+    else:
+        print("Using JIRA user name: ", email)
+
+    if not api_token:
+        print("Missing Jira API token")
+        exit(1)
+    return JIRA(
+        server="https://autonomic-ai.atlassian.net",
+        basic_auth=(email, api_token)
+    )
+
+
 class SprintUpdater:
     def __init__(self) -> None:
-        email = os.environ.get('JIRA_USER_NAME')
-        api_token = os.environ.get('JIRA_API_TOKEN')
-
-        if not email:
-            print("Missing Jira user name!")
-            exit(1)
-        else:
-            print("Using JIRA user name: ", email)
-
-        if not api_token:
-            print("Missing Jira API token")
-            exit(1)
-
-        self.jira = JIRA(
-            server="https://autonomic-ai.atlassian.net",
-            basic_auth=(email, api_token)
-        )
+        self.jira = create_session()
 
     def list_boards(self):
         boards = self.jira.boards()
         for b in boards:
             print(f"JIRA board: {b.name} ({b.id})")
 
-    def list_sprints(self, board_id, start_at: int = 0):
-        _sprints = self.jira.sprints(board_id=board_id, startAt=start_at)
+    def list_sprints(self, board_id, start_at: int = 0, max_results: int = 50):
+        _sprints = self.jira.sprints(board_id=board_id, startAt=start_at, maxResults=max_results)
         print(
             f"{len(_sprints)} sprints loaded. total={_sprints.total}, startAt={_sprints.startAt}, last={_sprints.isLast}")
         return _sprints
@@ -87,32 +91,35 @@ class SprintUpdater:
         else:
             print(f"Detected {_count - _done_issues} remaining issues in sprint {_sprint.name}")
 
+    def __reset_session(self):
+        self.jira.kill_session()
 
-updater = SprintUpdater()
-should_stop = False
-cutoff_date = dateutil.parser.isoparse("2022-11-28T00:00:00.000Z")
-jira_board_id = 51
+    def closeOverdueSprints(self, jira_board_id, cutoff_date: datetime.datetime):
+        offset = 0
+        max_results = 50
+        should_stop = False
+        while not should_stop:
+            sprints = self.list_sprints(jira_board_id, offset, max_results)
+            for s in sprints:
+                # print(f"sprint {s.id}-{s.name}")
+                has_date_info = s.raw.__contains__('startDate')
+                if has_date_info:
+                    start_date = dateutil.parser.isoparse(s.startDate)
+                    if start_date < cutoff_date:
+                        print(f"Checking sprint {s.name} (id:{s.id}, state:{s.state}, start date: {start_date.date()})")
+                        self.start_sprint(s.id)
+                        self.finish_sprint(s)
+                    else:
+                        print(f"future sprint {s.id}-{s.name} start date: {start_date.date()}")
+                else:
+                    print(f"sprint {s.id}-{s.name}: No date info")
 
-offset = 0
-maxResults = 50
-
-while not should_stop:
-    sprints = updater.list_sprints(jira_board_id, offset)
-    for s in sprints:
-        # print(f"sprint {s.id}-{s.name}")
-        hasDateInfo = s.raw.__contains__('startDate')
-        if hasDateInfo:
-            startDate = dateutil.parser.isoparse(s.startDate)
-            if startDate < cutoff_date:
-                print(f"Checking sprint {s.name} (id:{s.id}, state:{s.state}, start date: {startDate.date()})")
-                updater.start_sprint(s.id)
-                updater.finish_sprint(s)
+            if not sprints.isLast:
+                offset += max_results
             else:
-                print(f"future sprint {s.id}-{s.name} start date: {startDate.date()}")
-        else:
-            print(f"sprint {s.id}-{s.name}: No date info")
+                should_stop = True
 
-    if not sprints.isLast:
-        offset += 50
-    else:
-        should_stop = True
+
+end_date = dateutil.parser.isoparse("2022-11-28T00:00:00.000Z")
+updater = SprintUpdater()
+updater.closeOverdueSprints(jira_board_id=51, cutoff_date=end_date)
